b18d99bcb8ecb483d323e61e50060200fd731a2c
MargaridaMorais <maguy.1997@gmail.com>
2018-09-30 00:15:56 +0100

    Added function board_perform_move

--- untitled-1.py
+++ untitled-1.py
@@ -20,7 +20,7 @@ def make_pos (l, c):
 def pos_l (pos):
     return pos[0]
 def pos_c (pos):
-    return pos[1] 
+    return pos[1]
 
 # TAI move
 # Lista [p_initial, p_final]
@@ -29,7 +29,7 @@ def make_move (i,f):
 def move_initial (move):
     return move[0]
 def move_final (move):
-    return move[1] 
+    return move[1]
 
 def linhastab(tab):
     return len(tab)
@@ -60,19 +60,20 @@ def grupo(final,group):
         else:
             if i == group:
                 return True
-    
+
     if len(group) >= 1:
-        group = group[0]    
-    return False 
+        group = group[0]
+    return False
+
 def possiveisgruposlinhas(tab, pos, group):
-    
-    
+
+
     linha = pos_l(pos)
     coluna = pos_c(pos)
-    
-    if ((coluna < 0 or linha < 0) or linha > linhastab(tab)-1 or coluna > colunastab(tab)-1): 
+
+    if ((coluna < 0 or linha < 0) or linha > linhastab(tab)-1 or coluna > colunastab(tab)-1):
         return
-    
+
     #procuraesquerda
     colunacomp=coluna -2
     linhacomp=linha
@@ -81,9 +82,9 @@ def possiveisgruposlinhas(tab, pos, group):
             poscomp= make_pos(linhacomp, colunacomp)
             posmeio= make_pos(linhacomp, colunacomp+1)
             if(lista(group,poscomp) == False and is_peg(cor(tab,pos)) and is_peg(cor(tab, posmeio)) and is_empty(cor(tab, poscomp))):
-                group = [group + [poscomp]]               
+                group = [group + [poscomp]]
                 possiveisgruposlinhas(tab, poscomp, group)
-                
+
 
     #procuradireita
     colunacomp = coluna +2
@@ -93,9 +94,9 @@ def possiveisgruposlinhas(tab, pos, group):
             poscomp= make_pos(linhacomp, colunacomp)
             posmeio= make_pos(linhacomp, colunacomp-1)
             if(lista(group,poscomp) == False and is_peg(cor(tab, pos)) and is_peg(cor(tab, posmeio)) and is_empty(cor(tab, poscomp))):
-                    group = group + [[pos,poscomp]]                  
+                    group = group + [[pos,poscomp]]
                     possiveisgruposlinhas(tab, poscomp, group)
-                    
+
     #procurabaixo
     linhacomp= linha + 2
     colunacomp=coluna
@@ -114,8 +115,8 @@ def possiveisgruposlinhas(tab, pos, group):
                     print("noiceee")
                     group = group + [[pos, poscomp]]
                     possiveisgruposlinhas(tab, poscomp, group)
-                                      
-                                
+
+
     #procuracima
     linhacomp=linha-2
     colunacomp=coluna
@@ -125,9 +126,9 @@ def possiveisgruposlinhas(tab, pos, group):
             posmeio= make_pos(linhacomp+1, colunacomp)
             if(lista(group,poscomp) == False and is_peg(cor(tab, posmeio)) and is_peg(cor(tab, pos)) and is_empty(cor(tab, poscomp))):
                 group = [group + [poscomp]]
-                possiveisgruposlinhas(tab, poscomp, group)  
+                possiveisgruposlinhas(tab, poscomp, group)
                 print("gordo4")
-                            
+
     return group
 
 def checksize(group):
@@ -135,7 +136,8 @@ def checksize(group):
             if(len(group[i]) == 2 and isinstance(group[i], tuple)==False):
                 print(group[i])
                 print("AQUIII")
-                return False 
+                return False
+
 def board_moves(tab):
     final = []
     for i in range(0,linhastab(tab)):
@@ -143,12 +145,50 @@ def board_moves(tab):
             pos = make_pos(i,j)
             if(cor(tab,pos) != 0):
                 group = [pos]
-                group = possiveisgruposlinhas(tab,pos,group) 
-                if grupo(final,group) == False and checksize(group) == False: 
+                group = possiveisgruposlinhas(tab,pos,group)
+                if grupo(final,group) == False and checksize(group) == False:
                     print("entrei")
-                    final += group                
-                             
+                    final += group
+
     return final
 
+def board_perform_move(tab, move):
+    res = tab
+    initial_pos = move_initial(move)
+    final_pos = move_final(move)
+
+    if(pos_l(initial_pos) == pos_l(final_pos) and pos_c(initial_pos) != pos_c(final_pos)):
+        res[pos_l(initial_pos)][pos_c(initial_pos)] = '_'
+        
+        if(pos_c(initial_pos) > pos_c(final_pos)):
+            res[pos_l(initial_pos)][pos_c(initial_pos) - 1] = '_'
+            res[pos_l(initial_pos)][pos_c(final_pos)] = 'O'
+
+        elif(pos_c(initial_pos) < pos_c(final_pos)):
+            res[pos_l(initial_pos)][pos_c(initial_pos) + 1] = '_'
+            res[pos_l(initial_pos)][pos_c(final_pos)] = 'O'
+
+    elif(pos_l(initial_pos) != pos_l(final_pos) and pos_c(initial_pos) == pos_c(final_pos)):
+        if(pos_l(initial_pos) > pos_l(final_pos)):
+            res[pos_c(initial_pos)][pos_l(initial_pos) - 1] = '_'
+            res[pos_c(initial_pos)][pos_l(final_pos)] = 'O'
+
+        elif(pos_l(initial_pos) < pos_l(final_pos)):
+            res[pos_c(initial_pos)][pos_l(initial_pos) + 1] = '_'
+            res[pos_c(initial_pos)][pos_l(final_pos)] = 'O'
+
+    print('printing result')
+    for i in res:
+        print(i)
+    return res
 
 
+if __name__ == '__main__':
+    tab = [["_","O","O","O","_"], ["O","_","O","_","O"], ["_","O","_","O","_"],
+    ["O","_","O","_","_"], ["_","O","_","_","_"]]
+
+    for j in tab:
+        print(j)
+
+    moves = board_moves(tab)
+    board_perform_move(tab, moves[0])
